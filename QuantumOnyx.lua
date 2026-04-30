0cb74b5d9cde6fd0a13f0093ae32c55f98f0e402
ConsoleCrazeApp <angus2013lin1@gmail.com>
2026-04-30 16:20:55 -0400

    Update QuantumOnyx.lua

--- QuantumOnyx.lua
+++ QuantumOnyx.lua
@@ -1,5 +1,5 @@
 local Scripts = {
-    [994732206] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games/BloxFruits.lua",
+    [994732206] = "https://raw.githubusercontent.com/ConsoleCrazeApp/Copy-of-Quantum-Onyx-Project/refs/heads/main/BloxFruits.lua",
     [9186719164] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games/SailorPiece.lua",
 }
 
